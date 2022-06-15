4542b0691943fba057d89be84577217248ae61d5
Alejandro Saez <alejandrosaezc@gmail.com>
2022-06-15 04:51:29 +0200

    hola_mundo

---
+++ holamundo/holamundo/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+def saludo(request):
+    return HttpResponse("Hola Mundo")
+
+def despedida(request):
+    return HttpResponse("Adios Mundo")
+
+def adulto(request, edad):
+    if edad>=18:
+        return HttpResponse("Eres mayor de edad!")
+    else:
+        return HttpResponse("Eres menor!")
